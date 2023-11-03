bac2b4e004ec02eb60c2283063c78cda099d2e5c
Aj23Mah <ajaymaharjan758@gmail.com>
2023-11-03 16:42:35 +0545

    node conection with mongodb

---
+++ modules/config/db.config.js
@@ -0,0 +1,6 @@
+
+// admin123 => password, todo => folder name
+
+const DB_URL = "mongodb+srv://Aj_Mah:admin123@cluster0.q7fbsn8.mongodb.net/todo?retryWrites=true&w=majority"
+
+module.exports = DB_URL;
